bd5bef33293d7474ed8fc9c1161eb0a1ae3257e1
Diego Estrada <est21016@byui.edu>
2026-01-14 19:06:13 -0700

    updated the server.

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import express from 'express';
 // Create an instance of an Express application
 const app = express();
 
+const name = process.env.NAME; // 
+
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send(`Hello, ${name}!`); // <-- UPDATED
 });
 
 // Define the port number the server will listen on
