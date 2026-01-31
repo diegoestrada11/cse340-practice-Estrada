de7ced040d6c72ee0a3f5d0a31fd96387c9667a2
Diego Estrada <est21016@byui.edu>
2026-01-30 23:04:17 -0700

    Updated server

--- server.js
+++ server.js
@@ -68,16 +68,28 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
 /**
- * Global template variables middleware
- * 
- * Makes common variables available to all EJS templates without having to pass
- * them individually from each route handler
+ * Configure Express middleware
  */
+// Middleware to make NODE_ENV available to all templates
 app.use((req, res, next) => {
-    // Make NODE_ENV available to all templates
     res.locals.NODE_ENV = NODE_ENV.toLowerCase() || 'production';
-
     // Continue to the next middleware or route handler
+    next();
+});
+
+app.use((req, res, next) => {
+    // Skip logging for routes that start with /. (like /.well-known/)
+    if (!req.path.startsWith('/.')) {
+        
+    }
+    next(); // Pass control to the next middleware or route
+});
+
+// Middleware to add global data to all templates
+app.use((req, res, next) => {
+    // Add current year for copyright
+    res.locals.currentYear = new Date().getFullYear();
+
     next();
 });
 
